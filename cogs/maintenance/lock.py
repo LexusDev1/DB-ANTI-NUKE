3dc44579d4b907df7a0eceb6570f9f4174d33ec2
LexusDev <alan.aquebar@gmail.com>
2023-08-22 12:06:00 +0800

    Fifth commit by SKIBIB

--- cogs/maintenance/lock.py
+++ cogs/maintenance/lock.py
@@ -1,7 +1,7 @@
 import nextcord
 from nextcord.ext import commands
 
-class LockDown(commands.Bot):
+class LockDown(commands.Cog):
   def __init__(self, client):
     self.client = client
     
--- cogs/miscellaneous/ticket.py
+++ cogs/miscellaneous/ticket.py
@@ -1,4 +1,3 @@
-import nextcord
 import nextcord.ui 
 import nextcord
 from nextcord.ext import commands
@@ -17,10 +16,10 @@ async def ticketCallback(interaction):
       role: nextcord.PermissionOverwrite(view_channel=True)
   }
   select = Select(options=[
-          nextcord.SelectOption(label="Report Bug", emoji="🦟", description="Opens a ticket for bug report", value="01")
-          nextcord.SelectOption(label="Report Player", emoji="👾", description="Reports a player for cheating or abusing", value="02")
-          nextcord.SelectOption(label="Suggestions", emoji="💬", description="Opens a ticket for your suggestions on the server", value="03")
-          nextcord.SelectOption(label="Report Staff", emoji="🎃", description="Opens a ticket to report a staff whenever they abuse", value="04")
+          nextcord.SelectOption(label="Report Bug", emoji="🦟", description="Opens a ticket for bug report", value="01"),
+          nextcord.SelectOption(label="Report Player", emoji="👾", description="Reports a player for cheating or abusing", value="02"),
+          nextcord.SelectOption(label="Suggestions", emoji="💬", description="Opens a ticket for your suggestions on the server", value="03"),
+          nextcord.SelectOption(label="Report Staff", emoji="🎃", description="Opens a ticket to report a staff whenever they abuse", value="04"),
           nextcord.SelectOption(label="Application", emoji="📖", description="Opens a ticket for your application", value="05")
     ])
     
@@ -29,25 +28,81 @@ async def ticketCallback(interaction):
         category = nextcord.utils.get(guild.categories, name="TICKETS")
         channel = await guild.create_text_channel(f"{interaction.user.name}#{interaction.user.id}\n's ticket", category=category, overwrites=overwrites)
         embed = nextcord.Embed(description=f"Created ticket for {interaction.user.name} # <#{channel.id}>")
-        emrule = nextcord.Embed(
-              title="RULES",
-              description="HERE ARE THE **RULES** THAT YOU NEED TO OBEY ON YOUR TICKET IN ORDER TO GET A RESPOND",
-          )
-          emrule.add_field(name)
-        await interaction.response.send_message(embed=embed)
+        emrule = nextcord.Embed(title="RULES", description="HERE ARE THE RULES IN ORDER TO GET A RESPOND FROM A STAFF")
+        emrule.add_field(name="PING ONLY **1** STAFF", value="YOU CAN ONLY PING ONE STAFF IF NO RESPONSE", inline=False)
+        emrule.add_field(name="DO NOT MASSPING", value="MASS PINGING IS HIGHLY PROHIBITED AND CAN RESULT INTO NO RESPONSE", inline=False)
+        emrule.add_field(name="APPRECIATE THE HELP OF STAFF", value="ALWAYS **THANK** THE STAFF FOR HELPING YOU", inline=False)
+        emrule.add_field(name="DO NOT HARASS THE STAFF OR THE RESPONDENT", value="IF WE CAUGHT YOU HARASSING ONE OF OUR STAFF, WE WILL WARN YOU FOR HARASSING ONE", inline=False)
+        await interaction.response.send_message(embed=embed, ephemeral=True)
+        await interaction.followup.send(embed=emrule)
         await channel.send(f"<@{interaction.user.id}>")
       elif select.value[0] == "02":
         category = nextcord.utils.get(guild.categories, name="TICKETS")
+        channel = await guild.create_text_channel(f"{interaction.user.name}#{interaction.user.id}\n's ticket", category=category, overwrites=overwrites)
+        embed = nextcord.Embed(description=f"Created ticket for {interaction.user.name} # <#{channel.id}>")
+        emrule = nextcord.Embed(title="RULES", description="HERE ARE THE RULES IN ORDER TO GET A RESPOND FROM A STAFF")
+        emrule.add_field(name="PING ONLY **1** STAFF", value="YOU CAN ONLY PING ONE STAFF IF NO RESPONSE", inline=False)
+        emrule.add_field(name="DO NOT MASSPING", value="MASS PINGING IS HIGHLY PROHIBITED AND CAN RESULT INTO NO RESPONSE", inline=False)
+        emrule.add_field(name="APPRECIATE THE HELP OF STAFF", value="ALWAYS **THANK** THE STAFF FOR HELPING YOU", inline=False)
+        emrule.add_field(name="DO NOT HARASS THE STAFF OR THE RESPONDENT", value="IF WE CAUGHT YOU HARASSING ONE OF OUR STAFF, WE WILL WARN YOU FOR HARASSING ONE", inline=False)
+        await interaction.response.send_message(embed=embed, ephemeral=True)
+        await interaction.followup.send(embed=emrule)
+        await channel.send(f"<@{interaction.user.id}>")
       elif select.value[0] == "03":
         category = nextcord.utils.get(guild.categories, name="TICKETS")
+        channel = await guild.create_text_channel(f"{interaction.user.name}#{interaction.user.id}\n's ticket", category=category, overwrites=overwrites)
+        embed = nextcord.Embed(description=f"Created ticket for {interaction.user.name} # <#{channel.id}>")
+        emrule = nextcord.Embed(title="RULES", description="HERE ARE THE RULES IN ORDER TO GET A RESPOND FROM A STAFF")
+        emrule.add_field(name="PING ONLY **1** STAFF", value="YOU CAN ONLY PING ONE STAFF IF NO RESPONSE", inline=False)
+        emrule.add_field(name="DO NOT MASSPING", value="MASS PINGING IS HIGHLY PROHIBITED AND CAN RESULT INTO NO RESPONSE", inline=False)
+        emrule.add_field(name="APPRECIATE THE HELP OF STAFF", value="ALWAYS **THANK** THE STAFF FOR HELPING YOU", inline=False)
+        emrule.add_field(name="DO NOT HARASS THE STAFF OR THE RESPONDENT", value="IF WE CAUGHT YOU HARASSING ONE OF OUR STAFF, WE WILL WARN YOU FOR HARASSING ONE", inline=False)
+        await interaction.response.send_message(embed=embed, ephemeral=True)
+        await interaction.followup.send(embed=emrule)
+        await channel.send(f"<@{interaction.user.id}>")
       elif select.value[0] == "04":
         category = nextcord.utils.get(guild.categories, name="TICKETS")
+        channel = await guild.create_text_channel(f"{interaction.user.name}#{interaction.user.id}\n's ticket", category=category, overwrites=overwrites)
+        embed = nextcord.Embed(description=f"Created ticket for {interaction.user.name} # <#{channel.id}>")
+        emrule = nextcord.Embed(title="RULES", description="HERE ARE THE RULES IN ORDER TO GET A RESPOND FROM A STAFF")
+        emrule.add_field(name="PING ONLY **1** STAFF", value="YOU CAN ONLY PING ONE STAFF IF NO RESPONSE", inline=False)
+        emrule.add_field(name="DO NOT MASSPING", value="MASS PINGING IS HIGHLY PROHIBITED AND CAN RESULT INTO NO RESPONSE", inline=False)
+        emrule.add_field(name="APPRECIATE THE HELP OF STAFF", value="ALWAYS **THANK** THE STAFF FOR HELPING YOU", inline=False)
+        emrule.add_field(name="DO NOT HARASS THE STAFF OR THE RESPONDENT", value="IF WE CAUGHT YOU HARASSING ONE OF OUR STAFF, WE WILL WARN YOU FOR HARASSING ONE", inline=False)
+        await interaction.response.send_message(embed=embed, ephemeral=True)
+        await interaction.followup.send(embed=emrule)
+        await channel.send(f"<@{interaction.user.id}>")
       elif select.value[0] == "05":
         category = nextcord.utils.get(guild.categories, name="TICKETS")
+        channel = await guild.create_text_channel(f"{interaction.user.name}#{interaction.user.id}\n's ticket", category=category, overwrites=overwrites)
+        embed = nextcord.Embed(description=f"Created ticket for {interaction.user.name} # <#{channel.id}>")
+        emrule = nextcord.Embed(title="RULES", description="HERE ARE THE RULES IN ORDER TO GET A RESPOND FROM A STAFF")
+        emrule.add_field(name="PING ONLY **1** STAFF", value="YOU CAN ONLY PING ONE STAFF IF NO RESPONSE", inline=False)
+        emrule.add_field(name="DO NOT MASSPING", value="MASS PINGING IS HIGHLY PROHIBITED AND CAN RESULT INTO NO RESPONSE", inline=False)
+        emrule.add_field(name="APPRECIATE THE HELP OF STAFF", value="ALWAYS **THANK** THE STAFF FOR HELPING YOU", inline=False)
+        emrule.add_field(name="DO NOT HARASS THE STAFF OR THE RESPONDENT", value="IF WE CAUGHT YOU HARASSING ONE OF OUR STAFF, WE WILL WARN YOU FOR HARASSING ONE", inline=False)
+        await interaction.response.send_message(embed=embed, ephemeral=True)
+        await interaction.followup.send(embed=emrule)
+        await channel.send(f"<@{interaction.user.id}>")
+    select.callback = BotCallback
+    view = View(timeout=None)
+    view.add_item(select )
+    await interaction.response.send_message("**CHOOSE** an option below", view=view, ephemeral=True)
 
 class Ticket(commands.Cog):
   def __init__(self, client):
     self.client = client
+  
+  @nextcord.slash_command(description="Opens a ticket")
+  
+  async def ticket(self, interaction):
+    button = Button(label="📥 Create a Ticket", style=nextcord.ButtonStyle.green)
+    button.callback = ticketCallback
+    view = View(timeout=None)
+    view.add_item(button)
+    await interaction.response.send_message("Open a ticket below", view=view)
+    
+    
   
 def Ticket(client):
   client.add_cog(Ticket)
